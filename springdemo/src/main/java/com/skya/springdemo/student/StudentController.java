08e09d0bc1df227bac581d2a284be9495dad260d
Ashley-23 <ashleysatchivi92@gmail.com>
2024-08-02 22:21:58 +0200

    implement student services layers

--- springdemo/src/main/java/com/skya/springdemo/student/StudentController.java
+++ springdemo/src/main/java/com/skya/springdemo/student/StudentController.java
@@ -1,24 +1,27 @@
 package com.skya.springdemo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
-    @GetMapping
-    public List<String> findAllStudents(){
-        return List.of(
-                "Kokoe",
-                "Yasmine",
-                "Ashley",
-                "SATCHIVI"
 
-        );
+    private StudentService studentService = new StudentService();
+
+    private StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> findAllStudents(){
+        return studentService.findAllStudents();
     };
 
 
